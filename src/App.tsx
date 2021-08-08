6c3bb8f31b562185d32b912731f3d64e35f042d0
demon-monk <antdude@126.com>
2021-08-08 16:03:52 +0800

    basic editor with key command handle

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import {Editor, EditorState} from 'draft-js';
+import { Editor, EditorState, DraftEditorCommand, RichUtils } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 
 function App() {
-  const [editorState, setEditorState] = React.useState(
-    () => EditorState.createEmpty(),
+  const [editorState, setEditorState] = React.useState(() =>
+    EditorState.createEmpty(),
   );
   const onChange = (state: EditorState) => {
-    setEditorState(state)
-    console.log(state.getCurrentContent())
-  }
-  return <Editor editorState={editorState} onChange={onChange} />;
+    setEditorState(state);
+    console.log(state.getCurrentContent());
+  };
+
+  const handleKeyCmd = (
+    cmd: DraftEditorCommand,
+    state: EditorState,
+  ): 'handled' | 'not-handled' => {
+    const newState = RichUtils.handleKeyCommand(state, cmd);
+
+    if (newState) {
+      onChange(state);
+      return 'handled';
+    }
+    return 'not-handled';
+  };
+  return (
+    <Editor
+      editorState={editorState}
+      onChange={onChange}
+      handleKeyCommand={handleKeyCmd}
+    />
+  );
 }
 
-export default App
+export default App;
